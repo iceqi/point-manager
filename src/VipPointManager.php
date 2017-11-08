633752496865a8f204b65878d941afa42c8718dc
kai.liu <dms@444>
2017-11-08 11:14:46 +0800

    修改

--- src/VipPointManager.php
+++ src/VipPointManager.php
@@ -6,9 +6,12 @@
  * Time: 下午3:46
  */
 
-namespace Vbot\VipPointManager;
+namespace Iceqi\VipPointManager;
+
+use Hanson\Vbot\Contact\Groups;
 use Hanson\Vbot\Extension\AbstractMessageHandler;
 use Hanson\Vbot\Message\Text;
+use Hanson\Vbot\Support\Log;
 use Illuminate\Support\Collection;
 
 class VipPointManager extends AbstractMessageHandler
@@ -18,56 +21,78 @@ class VipPointManager extends AbstractMessageHandler
 
     public $version = '1.0';
 
-    public $name = 'point_manaer';
+    public $name = 'vip_point_manager';
 
     public $zhName = '积分管理';
 
     private static $array = [];
 
+    private $configs;
+
+    private $event_arr = [
+
+        '积分变动'=>[['增加','减少']],
+        '移除用户'=>[
+            'user'
+        ],
+        '查询积分'=>[
+            'user'
+        ],
+    ];
+    private $_operation = [
+        '+' => ['str' => '增加', 'remind' => '请继续保持哦'],
+        '-' => ['str' => '减少', 'remind' => '请注意您的积分哦']
+    ];
+
     public function handler(Collection $message)
     {
         if ($message['type'] === 'text' && $message['fromType'] === 'Group') {
-            $username = $message['from']['UserName'];
 
-            $isBegin = isset(static::$array[$username]);
-
-            if ($message['pure'] === '查积分') {
-                if ($isBegin) {
-                    Text::send($username, '猜数字已经开始，还没结束呢');
-                    Text::send($username, '当前区间为：'.static::$array[$username]['begin'].' 到 '.static::$array[$username]['end']);
-                } else {
-                    Text::send($username, '猜数字开始，请猜一个 1 ~ 99 的数字，中了就赢了哦');
-                    static::$array[$username] = [
-                        'begin'  => 0,
-                        'end'    => 100,
-                        'target' => random_int(1, 100),
-                    ];
-                }
-            } elseif (is_numeric($message['content']) && $isBegin) {
-                $message['content'] = intval($message['content']);
-                $target = static::$array[$username]['target'];
-                if ($message['content'] > static::$array[$username]['begin'] && $message['content'] < static::$array[$username]['end']) {
-                    if ($message['content'] == $target) {
-                        Text::send($username, $message['sender']['NickName'].'你赢了！数字就为：'.static::$array[$username]['target']);
-                        unset(static::$array[$username]);
-                    } elseif ($message['content'] > $target) {
-                        Text::send($username, '当前区间为：'.static::$array[$username]['begin'].' 到 '.$message['content']);
-                        static::$array[$username]['end'] = $message['content'];
-                    } else {
-                        Text::send($username, '当前区间为：'.$message['content'].' 到 '.static::$array[$username]['end']);
-                        static::$array[$username]['begin'] = $message['content'];
+            $group_name = $message['from']['NickName'];  //群名
+            $nickname = $message['sender']['NickName']; // 发消息的用户
+            $real_name = $message['sender']['RemarkName']; // 好友备注名称
+            $group_id = $message['from']['UserName']; //群组id
+            if (isset($this->config['groups'][$group_name]) && is_array($this->config['groups'][$group_name])) {
+                if ($real_name == $this->config['groups'][$group_name]['manager']) {
+                    $event_user = explode(' ', $message['pure']);
+                    if (count($event_user) > 0) {
+                        $event = $event_user[0];
+                        if (in_array($event, $this->event_arr)) {
+                            $operation = substr($event_user[1], -2, 1);
+                            $point = substr($event_user[1], -1, 1);
+                            $user = str_replace($point, '', str_replace($operation, '', $event_user[1]));
+                            if ($user_point = explode(':', $user)) {
+                                Text::send($group_id, "{$user} 管理员 ({$real_name}) {$this->_operation[$operation]['str']}了【{$point}】积分，{$this->_operation[$operation]['remind']}");
+                            }
+                        }
                     }
+                }
+            } else {
+                if ($message['pure'] === '查积分') {
+                    Text::send($group_id, "@{$nickname} 积分查询成功");
                 }
             }
         }
+
     }
+
+    public function operation()
+    {
+
+    }
+
+    public function execute()
+    {
+
+    }
+
 
     /**
      * 注册拓展时的操作.
      */
     public function register()
     {
-
+        $this->configs = vbot('config')->get('extension.' . $this->name);
     }
 
 }
